db9e0638cb79d087244b07d1964075aed47c8742
isavieira07 <164920424+isavieira07@users.noreply.github.com>
2025-07-30 13:36:52 -0300

    n

---
+++ script.js
@@ -0,0 +1,6 @@
+function inserirNome(){
+    let nomeUsusrio = prompt("Qual o seu nome");
+    let elemento = document.querySelector("#nome-usuario");
+    console.log(elemento);
+    elemento.textContent = nomeUsuario;
+}
