a829838d84b2f23092b8cbb23bfaf45698d8fe22
isavieira07 <164920424+isavieira07@users.noreply.github.com>
2025-08-01 15:17:46 -0300

    s

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function inserirNome(){
     console.log(elemento);
     elemento.textContent = nomeUsuario;
 }
+
+const lista = document.querySelector('#lista');
+
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagens[2];
+
+let aluno1 = {
+    nome: 'Isabella',
+    idade: 17,
+    anoLetivo: 'Ensino Médio',
+    materiasFavoritas: ['Português', 'Programação']
+}
+
+console.log(aluno1.nome);
+console.log(aluno1.idade);
+console.log(aluno1.materiasFavoritas);
+console.log(aluno1.materiasFavoritas[1]);
